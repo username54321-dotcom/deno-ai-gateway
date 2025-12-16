63afe2248f2766b10b3c2e8e5aa9ac17822312e8
username54321-dotcom <marwankam3l@gmail.com>
2025-12-16 17:46:58 +0200

    add get providor helper function

---
+++ utils/getProvidor.ts
@@ -0,0 +1,17 @@
+import { kv } from "../clients/kv.ts";
+
+type Providors = "groq" | "openrouter";
+const providors = ["groq", "openrouter"] as const;
+
+export async function getProvidor(): Promise<Providors> {
+  const currentProvidorIndex = await kv
+    .get<number>(["currentProvidor"])
+    .then((res) => res.value ?? 0);
+
+  const newProvidorIndex =
+    currentProvidorIndex + 1 < providors.length ? currentProvidorIndex + 1 : 0;
+
+  await kv.set(["currentProvidor"], newProvidorIndex);
+
+  return providors[currentProvidorIndex];
+}
